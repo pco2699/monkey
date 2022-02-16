7a0b16da4cd819ace24b6f0dfe300b414fbdfde8
pco2699 <aenkun@gmail.com>
2022-02-16 21:39:38 +0900

    first commit

---
+++ lexer/lexer_test.go
@@ -0,0 +1,30 @@
+package lexer
+
+import (
+	"testing"
+
+	"interpreter/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+	for i, tt := range tests {
+
+	}
+}
