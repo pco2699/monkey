afeb1638dfc6cdd839adefa4f2e4bfe0d5f464b7
pco2699 <aenkun@gmail.com>
2022-02-22 09:49:44 +0900

    update

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -3,7 +3,7 @@ package lexer
 import (
 	"testing"
 
-	"interpreter/token"
+	"monkey/token"
 )
 
 func TestNextToken(t *testing.T) {
@@ -25,6 +25,16 @@ func TestNextToken(t *testing.T) {
 
 	l := New(input)
 	for i, tt := range tests {
+		tok := l.NextToken()
 
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got %q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] = literal wrong. expected=%q, got %q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
 	}
 }
